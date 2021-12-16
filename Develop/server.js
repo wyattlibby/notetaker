30faaab5a4c613a854eebc7db8f029d2672139b3
Brad <kingdomlandscapevt@gmail.com>
2021-12-16 14:12:49 -0500

    added html routes

--- Develop/server.js
+++ Develop/server.js
@@ -8,3 +8,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+//html routes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+app.get("/api/notes", (req, res) => {
+  return res.sendFile(path.json(__dirname, "db/db.json"));
+});
