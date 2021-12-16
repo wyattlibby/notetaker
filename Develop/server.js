55d4e05edd109b1c3a316f051c2d9bad302dda0c
Brad <kingdomlandscapevt@gmail.com>
2021-12-16 14:53:56 -0500

    finished delete route

--- Develop/server.js
+++ Develop/server.js
@@ -17,7 +17,7 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 app.get("/api/notes", (req, res) => {
-  return res.sendFile(path.json(__dirname, "db/db.json"));
+  return res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
 app.get("/api/notes", (req, res) => {
@@ -29,10 +29,18 @@ app.post("/api/notes", (req, res) => {
     const notes = readData();
     const newNote = req.body;
     newNote.id = Date.now();
+    notes.push(newNote);
+    writeData(notes);
+    res.status(200).send();
   	
 });
 app.delete("/api/notes/:id", (req, res) => {
 	//delete note matching id
+    const id = Number(req.params.id);
+    const notes = readData();
+    const newNotes = notes.filter(note => note.id !== id);
+    writeData(newNotes);
+    res.status(200).send();
   	
 });
 
